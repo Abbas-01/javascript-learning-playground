af04be64a560d21e4a17aef81747eb6b4d027e0e
Abbas 01 <abbashere786@gmail.com>
2023-11-13 01:03:09 +0500

    Creating a Watch

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-function itemList(arr, item) {
-    arr.shift();
-    arr.push(item);
+function watch() {
+    let sec = 0;
+    let min = 0;
+    let hour = 0;
+    setInterval(() => {
+        sec++;
+        if (sec === 60) {
+            sec = 0;
+            min++;
+            if (min === 60) {
+                min = 0;
+                hour++;
+                console.log("Hours" + hour);
+            }
+            console.log("Here" + min);
+        }
+        console.log(sec)
+    }, 10);
+    
 }
-
-let itemsArr = ["Chips", "Coke", "Chicken", "Chocolate"];
-
-console.log(itemsArr);
-
-itemList(itemsArr, "Burger");
-
-console.log(itemsArr);
+watch();
