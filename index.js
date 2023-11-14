67f5465a1ca637d02beafa9f6e4d828552a52c47
Abbas 01 <abbashere786@gmail.com>
2023-11-14 18:14:32 +0500

    Practicing switch statment

--- index.js
+++ index.js
@@ -1,51 +1,20 @@
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home"];
-/*
-function titlesForGolfPlayer (par, strokes) {
-    if (strokes === 1) {
-        return names[0];
-    } else if (strokes <= 3) {
-        return names[1];
-    } else if (strokes === par - 1) {
-        return names[2];
-    } else if (strokes === par) {
-        return names[3];
-    } else if (strokes === par + 1) {
-        return names[4];
-    } else if (strokes === par + 2) {
-        return names[5];
-    } else if (strokes > par + 3) {
-        return names[6];
-    } 
-}
-*/
-titlesForGolfPlayer(5, 3);
-
-function titlesForGolfPlayer(par, strokes) {
-    switch (strokes) {
+function hello(val) {
+    switch (val) {
         case 1 : 
-            console.log(names[0]);
+        console.log("Alpha");
         break;
         case 2 :
+            console.log("Beta");
+        break;
         case 3 :
-            console.log(names[1]);
+            console.log("Gamma");
         break;
-        case par - 1 :
-            console.log(names[2]);
-        break;
-        case par :
-            console.log(names[3]);
-        break;
-        case par + 1 :
-            console.log(names[4]);
-        break;
-        case par + 2 :
-            console.log(names[5]);
+        case 4 :
+            console.log("Delta");
         break;
         default :
-        if (strokes >= par + 3) {
-            console.log(names[6]);
-        }
+        console.log("Error!");
         break;
     }
 }
-titlesForGolfPlayer(5, 10);
+hello(1)
