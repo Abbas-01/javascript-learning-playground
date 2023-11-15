4ddef355c4cfd5a654d57a611b00fd6d7530210f
Abbas 01 <abbashere786@gmail.com>
2023-11-15 16:17:45 +0500

    Practicing converting if/else into switch

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 function hello(pra) {
     let x;
+
     switch (pra) {
-        case 1 :
-        case 2 :
-        case 3 :
-           x = "Low";
+        case "bob" : 
+        x = "Marley";
         break;
-        case 4 :
-        case 5 :
-        case 6 :
-           x = "Medium";
+        case 42 :
+            x = "The Answer";
+        break;
+        case 1 :
+            x = "There is no #1";
+        break;
+        case 99 :
+            x = "Missed me by this much!";
         break;
         case 7 :
-        case 8 :
-        case 9 :
-           x = "High";
-        break;
-        default :
-           x = "Out of the Limit";
+            x = "Ate Nine";
         break;
     }
     return x;
-    }
-    console.log(hello(2)); // Output "Low" 
+}
+
+console.log(hello(99));
