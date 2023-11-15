0779c772253565b58dc107a8bce36a8b4bb7ffb4
Abbas 01 <abbashere786@gmail.com>
2023-11-15 16:04:20 +0500

    Practicing switch with different methods

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
 function hello(pra) {
-let x;
-switch (pra) {
-    case 1 :
-       x = "Low";
-    break;
-    case 2 :
-       x = "Low";
-    break;
-    case 3 :
-       x = "Low";
-    break;
-    case 4 :
-       x = "Medium";
-    break;
-    case 5 :
-       x = "Medium";
-    break;
-    case 6 :
-       x = "Medium";
-    break;
-    case 7 :
-       x = "High";
-    break;
-    case 8 :
-       x = "High";
-    break;
-    case 9 :
-       x = "High";
-    break;
-    default :
-       x = "Out of the Limit";
-    break;
-}
-return x;
-}
-console.log(hello(5));
+    let x;
+    switch (pra) {
+        case 1 :
+        case 2 :
+        case 3 :
+           x = "Low";
+        break;
+        case 4 :
+        case 5 :
+        case 6 :
+           x = "Medium";
+        break;
+        case 7 :
+        case 8 :
+        case 9 :
+           x = "High";
+        break;
+        default :
+           x = "Out of the Limit";
+        break;
+    }
+    return x;
+    }
+    console.log(hello(2)); // Output "Low" 
