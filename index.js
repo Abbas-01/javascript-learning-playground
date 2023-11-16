6679922c644af1e0d326a76a0a8717937d0fd334
Abbas 01 <abbashere786@gmail.com>
2023-11-16 16:24:13 +0500

    Practicing with Card Counting

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-let names = ["Hole-in-one", "Eagle", "Birdie", "Par", "Boogey", "Double-Boogey", "Go Home"];
-let par = 8;
+let count = 0;
 
-function helo(stroke) {
-    if (stroke === 1) {
-        return names[0];
-    } else if (stroke <= 4) {
-        return names[1];
-    } else if (stroke <= 7) {
-        return names[2];
-    } else if (stroke === par) {
-        return names[3];
-    } else if (stroke  <= par + 2 ) {
-        return names[4];
-    } else if (stroke <= par + 4) {
-        return names[5];
-    }  else if (stroke > par + 4) {
-        return names[6];
-    } else if (typeof stroke !== "number") {
-        return "Only Numbers are Valid characters."
+function helo(card) {
+
+    switch (card) {
+        case 2 :
+        case 3 :
+        case 4 :
+        case 5 :
+            count++;
+        break;
+
+        case 8 :
+        case 9 :
+        case 10 :
+        case "K" :
+        case "A" :
+            count--;
+        break;
     }
+
+    let betGuide;
+    if (count <= 0) {
+        betGuide = "Hold";
+    } else {
+        betGuide = "Bet";
+    }
+
+    return count + " " + betGuide;
 }
-console.log(helo("string"))
+ helo(2);
+ helo(8);
+ console.log(helo("K"));
