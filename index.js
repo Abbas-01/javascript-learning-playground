0997a9eddf5227d7a5ae9826d2e1491d7d52c7bb
Abbas 01 <abbashere786@gmail.com>
2023-11-17 16:45:07 +0500

    Manipulating Complex Arrays

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-let person = {
-    about : {
-        name : "Abbas",
-        age : 16,
-        city: "Matli"
-    },
-    work : {
-        class : "Matric",
-        school : "AIMS",
-        skills : {
-            languages : ["HTMl", "CSS", "Javascript"],
-            learning : "Javascript"
-        }
-    }
-}
+let nestArr = [
+    [ "Hello",
+        {
+            name: "Abbas",
+            age : 16,
+            class : "Matric",
+            city : "Matli",
+            languages: ["HTML", "CSS", "JavaScript"]
+        },
+        ["Pakistan", "Asia", "Earth", "MilkyWay"]
+    ], [
+        "Welcome",
+        {
+            name : "Asad",
+            age : 16,
+            class : "Matric",
+            city : "Matli",
+            languages : ["HTML", "CSS"],
+        },
+        ["Pakistan", "Asia", "Earth", "MilkyWay"]
+    ]
+];
 
-console.log(person.work.skills.languages[1]);
+console.log(`${nestArr[0][0]} ${nestArr[0][1].name} ${nestArr[0][1].languages[1]} ${nestArr[0][1].city} ${nestArr[0][2][0]}`);
+console.log(`${nestArr[0][0]} ${nestArr[1][1].name} ${nestArr[1][1].class} ${nestArr[1][1].city} ${nestArr[1][2][0]}`)
