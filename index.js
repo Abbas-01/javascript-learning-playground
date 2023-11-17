5719badf9f90ce1671a61e0ac3d378b0914dc7e8
Abbas 01 <abbashere786@gmail.com>
2023-11-17 17:50:13 +0500

    Practicin with Record Collection

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-let nestArr = [
-    [ "Hello",
-        {
-            name: "Abbas",
-            age : 16,
-            class : "Matric",
-            city : "Matli",
-            languages: ["HTML", "CSS", "JavaScript"]
-        },
-        ["Pakistan", "Asia", "Earth", "MilkyWay"]
-    ], [
-        "Welcome",
-        {
-            name : "Asad",
-            age : 16,
-            class : "Matric",
-            city : "Matli",
-            languages : ["HTML", "CSS"],
-        },
-        ["Pakistan", "Asia", "Earth", "MilkyWay"]
-    ]
-];
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
 
-console.log(`${nestArr[0][0]} ${nestArr[0][1].name} ${nestArr[0][1].languages[1]} ${nestArr[0][1].city} ${nestArr[0][2][0]}`);
-console.log(`${nestArr[0][0]} ${nestArr[1][1].name} ${nestArr[1][1].class} ${nestArr[1][1].city} ${nestArr[1][2][0]}`)
+var collectionCopy = JSON.parse(JSON.stringify(recordCollection));
+  
+  function upadateCollection (id, prop, value) {
+      if (value === "") {
+          delete recordCollection[id][prop];
+    } else if (prop === "tracks") {
+        recordCollection[id][prop] = recordCollection[id][prop] || [] ;
+         recordCollection[id][prop].push(value);
+    } 
+    return recordCollection;
+}
+
+upadateCollection(1245, "tracks", "Abbas");
+upadateCollection(2468, "artist", "Ali");
+console.log(recordCollection);
