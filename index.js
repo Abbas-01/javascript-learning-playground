e72444552d17f91e010c508bc89388d751120f75
Abbas 01 <abbashere786@gmail.com>
2023-11-18 17:15:16 +0500

    Practicing nested loops

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let arr = [1, 2, 3, 4, 5];
-let zero = "0";
-for (i = 0; i < arr.length; i++) {
-  arr[i] = zero + arr[i];
+let myArr = [[1, 5], [2,6], [3, 7], [4, 8]];
+
+for (let i = 0; i < myArr.length; i++) {
+  for (let j = 0; j < myArr[i].length; j++) {
+    console.log(myArr[i][j]);
+  } 
 }
-console.log(arr)
