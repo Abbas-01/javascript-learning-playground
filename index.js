fa55205cff4075b74ed6bc977522995d8b948888
Abbas 01 <abbashere786@gmail.com>
2023-11-18 16:23:04 +0500

    Practicing for loops

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let helloArr = [];
 
-for (let i = 4; i >= 0; i--) {
-  helloArr.push(i);
-}
+for (let i = 0; i <= 10; i++) {
+  
+  if (i % 2 === 0) {
 
+    helloArr.push(i);
+  }
+}
 console.log(helloArr)
