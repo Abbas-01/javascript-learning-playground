cc126f630c5b72a26f0475a704d70d3256e68ed9
Abbas 01 <abbashere786@gmail.com>
2023-11-18 18:14:13 +0500

    Practicing with lookupProfile

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-let i = 2;
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
 
-// do..while loop
-do {
-    console.log(i);
-    i--;
-}while (i > 5) //Output : 2
-
-// while 
-while (i > 5) {
-  console.log(i);
-    i--;
-} // Output : nothing
+function lookupFunc(name, prop) {
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name){
+      return contacts[i][prop];
+    }
+}
+}
+let data = lookupFunc("Akira", "likes");
+console.log(data);
