2276ed1d22c54e4d8325d524236643e154be0fd8
Abbas 01 <abbashere786@gmail.com>
2023-11-18 17:31:32 +0500

    Modifying Array using nested loops

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let myArr = [[1, 5], [2,6], [3, 7], [4, 8]];
-
+function productArr() {
 for (let i = 0; i < myArr.length; i++) {
   for (let j = 0; j < myArr[i].length; j++) {
-    console.log(myArr[i][j]);
-  } 
+    myArr[i][j] = myArr[i][j] + 2;
+  }
 }
+return myArr;
+}
+
+console.log(productArr())
