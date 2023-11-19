486a12eb3a2943a9dd48b6fcc4d53addd75675dd
Abbas 01 <abbashere786@gmail.com>
2023-11-19 17:24:24 +0500

    Practicing Math Object's pow method

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-let x = Math.round(4.3);
-console.log(x); // Output : 4
-
-let y = Math.round(43.5);
-console.log(y); // Output : 44
-
-let z = Math.round(32.8);
-console.log(z); // Output : 33
+let x = Math.pow(2, 3);
+console.log(x); // Output : 8
+let y = Math.pow(5, 2);
+console.log(y); // Output : 25
+let z = Math.pow(4, 5);
+console.log(z); // Output : 1024 
