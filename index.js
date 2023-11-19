2ec1ecc2c01cfae71fc035181fdb0bf9051753e9
Abbas 01 <abbashere786@gmail.com>
2023-11-19 17:10:04 +0500

    Practicing Math Object's methods

--- index.js
+++ index.js
@@ -1,54 +1,8 @@
-const contacts = [
-    {
-      firstName: "Akira",
-      lastName: "Laine",
-      number: "0543236543",
-      likes: ["Pizza", "Coding", "Brownie Points"],
-    },
-    {
-      firstName: "Harry",
-      lastName: "Potter",
-      number: "0994372684",
-      likes: ["Hogwarts", "Magic", "Hagrid"],
-    },
-    {
-      firstName: "Sherlock",
-      lastName: "Holmes",
-      number: "0487345643",
-      likes: ["Intriguing Cases", "Violin"],
-    },
-    {
-      firstName: "Kristian",
-      lastName: "Vos",
-      number: "unknown",
-      likes: ["JavaScript", "Gaming", "Foxes"],
-    },
-  ];
+let x = Math.round(4.3);
+console.log(x); // Output : 4
 
-//   Looking Up using loops : Simple and Good!
-function helo2(name, prop) {
-    for (let i = 0; i < contacts.length; i++) {
-        if (contacts[i].firstName === name) {
-            return contacts[i][prop];
-        } 
-    }
-}
-console.log(helo("Sherlock", "likes"));
+let y = Math.round(43.5);
+console.log(y); // Output : 44
 
-//   Looking Up without using loops : Code repeating Bad! 
-
-  function helo(name, prop) {
-    if (contacts[0].firstName === name) {
-        return contacts[0][prop]
-    } else if (contacts[1].firstName === name) {
-        return contacts[1][prop]
-    } else if (contacts[2].firstName === name) {
-        return contacts[2][prop]
-    } else if (contacts[3].firstName === name) {
-        return contacts[3][prop]
-    } 
-  }
-  console.log(helo("Kristian", "likes"));
-
-
-  
+let z = Math.round(32.8);
+console.log(z); // Output : 33
