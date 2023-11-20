835400885d1b6b941ca52624e0cd265466b4ba75
Abbas 01 <abbashere786@gmail.com>
2023-11-20 13:12:33 +0500

    Practicing Multiple Ternary operators

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function helo(number) {
-    let x = number < 5 ? "Less than five" : "Greater than five";
+    let x = number < 5 ? "Less than five" : number > 5 ? "Greater than five" : "Equal to five";
     return x;
 }
+console.log(helo(3));
+console.log(helo(5));
 console.log(helo(10));
