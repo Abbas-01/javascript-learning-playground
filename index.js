9598bad7e73875a7379aa252ddf61716a83dc715
Abbas 01 <abbashere786@gmail.com>
2023-11-20 18:52:10 +0500

    Practicing Pass by refernce

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let x = [1, 2, 3];
-console.log(x);
+/* jo array parameter hai wo aur x variable dono ka aik hi reference tha memory ma to phr hamna array parameter ko jab equal/assignment operator lga kr alag sa aik value assign ki to isna aik new location create kr li memory ma  */
 
-let y = x;
-y.push(5);
-console.log(x)
-console.log(y);
-
-y = [5, 6, 7]
-console.log(y);
-console.log(x)
+let x =  [1, 2];
+console.log(x); // Returns [1, 2]
+add(x, 4);
+console.log(x); // Returns [1, 2]
+function add(array, element) {
+    array = [element]
+    console.log(array); // Returns [4]
+};
