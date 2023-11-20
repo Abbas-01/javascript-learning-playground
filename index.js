f6f37b9490c1fa39248cc932a65aaf295e0d4ffd
Abbas 01 <abbashere786@gmail.com>
2023-11-20 14:04:31 +0500

    Const variable

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-// Using var Keyword
-var hello = 2;
-var hello = 5;
-console.log(hello); // Returns 5
-console.log(hello); // Returns 5
-
-// Using let Keyword
-let hello = 2;
-let hello = 5;
-console.log(hello); // Returns Error
-console.log(hello); // Returns Error
-
+const x =3;
+console.log(x)
+x = 1;
+console.log(x)
