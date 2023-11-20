6440b11dc6c205f698b3f27d10215dc7e1971f71
Abbas 01 <abbashere786@gmail.com>
2023-11-20 13:10:18 +0500

    Practicing Ternary Operators

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-function helo(number, power) {
-    let x;
-    switch (power) {
-        case "one":
-          x = 1; 
-        break;
-        case "two":
-            x = 2; 
-          break;
-        case "three":
-            x = 3; 
-          break;
-        case "four":
-            x = 4; 
-          break;
-
-
-    }
-    let value = Math.pow(number, x);
-    return value;
-    
+function helo(number) {
+    let x = number < 5 ? "Less than five" : "Greater than five";
+    return x;
 }
-console.log(helo(4, "four"));
-console.log(helo(2, "four"));
-console.log(helo(3, "three"));
-console.log(helo(1023, "four"));
+console.log(helo(10));
