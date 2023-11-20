2a778b1abe3d453c78a65b878f14c18e47d1dd8e
Abbas 01 <abbashere786@gmail.com>
2023-11-20 10:28:31 +0500

    Generating Random Numbers Within a Range

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-let y = Math.floor(Math.random() * 6);
-let w = Math.ceil(Math.random() * 6);
-let x = Math.floor(Math.random() * 5) + 1;
-let z = Math.ceil(Math.random() * 5) + 1;
-console.log(x);
-console.log(w);
-console.log(y);
-console.log(z);
+
+function randRange(max, min) {
+    let randnumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randnumber
+}
+console.log(randRange(6, 2));
