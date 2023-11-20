13a77ca787ac63664eaa74f9f0a04b56a6726693
Abbas 01 <abbashere786@gmail.com>
2023-11-20 08:58:46 +0500

    Practicing Math Object with random method

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let x = Math.pow(2, 3);
-console.log(x); // Output : 8
-let y = Math.pow(5, 2);
-console.log(y); // Output : 25
-let z = Math.pow(4, 5);
-console.log(z); // Output : 1024 
+let y = Math.floor(Math.random() * 6);
+let w = Math.ceil(Math.random() * 6);
+let x = Math.floor(Math.random() * 5) + 1;
+let z = Math.ceil(Math.random() * 5) + 1;
+console.log(x);
+console.log(w);
+console.log(y);
+console.log(z);
