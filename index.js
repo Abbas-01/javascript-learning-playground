4267959672c86db453c562e39dbe0e971777bc5b
Abbas 01 <abbashere786@gmail.com>
2023-11-23 23:41:13 +0500

    Practicing with FizzBuzz

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-// const names = ["Abbas", "Ali", "Asad", "Azlan", "Azan"];
-// const [one, two, ...three] = names;
-// console.log(three)
-const obj = {
-    name: "Abbas",
-    fatherName: "Ashiq",
-    age: 16
+function helo(num) {
+    if (typeof num === "number") {
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0) {
+            console.log("Fizz")
+        } else if (i % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        } 
+    }
+} else {
+    console.log("Numbers are only valid character")
 }
-const {name, fatherName: father, hello = "Matric", age} = obj;
-console.log(father);
-console.log(age);
-console.log(hello);
+} 
+helo(30);
+// console.log(171 % 3)
