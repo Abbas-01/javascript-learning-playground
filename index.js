c8e34b00144916801083deecc8f9647639d2a3f1
Abbas 01 <abbashere786@gmail.com>
2023-11-23 18:43:30 +0500

    practicing destructuring assignment with objects

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const names = ["Abbas", "Ali", "Asad", "Azlan", "Azan"];
-const [, two, , four] = names;
-console.log(two)
-console.log(four)
+// const names = ["Abbas", "Ali", "Asad", "Azlan", "Azan"];
+// const [one, two, ...three] = names;
+// console.log(three)
+const obj = {
+    name: "Abbas",
+    fatherName: "Ashiq",
+    age: 16
+}
+const {name, fatherName: father, hello = "Matric", age} = obj;
+console.log(father);
+console.log(age);
+console.log(hello);
