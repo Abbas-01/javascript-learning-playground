c2fcfc8d8e10ea9cd932904fd45f87828010d533
Abbas 01 <abbashere786@gmail.com>
2023-11-23 16:07:55 +0500

    Generating Random Even Numbers

--- index.js
+++
@@ -1,7 +0,0 @@
-const x = (first, last, arg) => {
-    let fullName = first + " " + last;
-    // let modArrr = arg.map(x => "Hello " + x);
-    let hello = arg.pop();
-    return [fullName, hello] 
-}
-console.log(x("Asad", "Chang", ["Matric", "Matli"]));
