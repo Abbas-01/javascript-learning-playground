c4cd89f231a8c40954f348f922a742e2b97f4b98
Abbas 01 <abbashere786@gmail.com>
2023-11-24 14:40:48 +0500

    Practicing Destructuring Assignments with object and func

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const AVG_Tempearture = {
     today: 54.2,
     tomorrow: 21.7
 }
-const helo = ({today, tomorrow}) => {
-    const todayTemp = tomorrow;
-    return todayTemp;
+const helo = (temperature) => {
+    const {today : todayTemp, tomorrow : tomoTemp} = temperature;
+    return tomoTemp;
 }
 console.log(helo(AVG_Tempearture));
