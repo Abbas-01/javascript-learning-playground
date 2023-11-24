a777c56249b67c0432100334c9bfbe7ac0e11223
Abbas 01 <abbashere786@gmail.com>
2023-11-24 16:33:07 +0500

    Praciticng Template literals

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ let arr = {
 
 function myArr(x) {
     let z = [];
-    z.push(...x);
+    let numIden;
+    if (x === arr.a) {
+        numIden = "Cardinal"
+    } else if (x === arr.b) {
+        numIden = "Natural"
+    } else if (x === arr.c) {
+        numIden = "Roman"
+    }
+    for (let i = 0; i < x.length; i++) {
+    z.push(`This is ${x[i]} ${numIden} Number`);
+    }
     return z;
 }
-console.log(myArr(arr.c));
+console.log(myArr(arr.a));
