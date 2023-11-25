538a344a2c4c2a87c3307402a74e145a09554510
Abbas 01 <abbashere786@gmail.com>
2023-11-25 16:56:25 +0500

    Practicing pre incrementing operator

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-function info(name, age, grade, school) {
-    return {
-        name,
-        age,
-        grade,
-        school
-    }
-}
+// function helo(num) {
+//     return function () {
+//         return num++;
+//     }
+// }
+// let counterFunc = helo(5);
+// console.log(counterFunc())
+// console.log(counterFunc())
+// console.log(counterFunc())
+// console.log(counterFunc())
+// console.log(counterFunc())
+// console.log(counterFunc())
 
-const abbas = info("Abbas", 15, "Matric", "AIMS"); 
-console.log(abbas);
-
-const asad = info("Asad", 16, "Matric", "AIMS"); 
-console.log(asad);
-
-const farzain = info("Farzain", 16, "Matric", "The Smart School"); 
-console.log(farzain);
-
-const firstName = "Abbas";
-const age = 16;
-
-const person = {
-    firstName,
-    age
-}
-
-console.log(person.firstName);
-console.log(person.age);
+let a = 1;
+console.log(a); 
+console.log(++a);
