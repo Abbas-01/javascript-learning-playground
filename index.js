78b5515b3545812aae0b6d35b552e02eba3c701b
Abbas 01 <abbashere786@gmail.com>
2023-11-25 12:06:06 +0500

    Practicing Factory Function using Destructuring

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function info(name, age, grade, school) {
     return {
-        name: name,
-        age: age,
-        grade: grade,
-        school: school
+        name,
+        age,
+        grade,
+        school
     }
 }
 
 const abbas = info("Abbas", 15, "Matric", "AIMS"); 
-const asad = info("Asad", 16, "Matric", "AIMS"); 
-const farzain = info("Farzain", 16, "Matric", "The Smart School"); 
 console.log(abbas);
+
+const asad = info("Asad", 16, "Matric", "AIMS"); 
 console.log(asad);
+
+const farzain = info("Farzain", 16, "Matric", "The Smart School"); 
 console.log(farzain);
+
