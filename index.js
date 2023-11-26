1538e7c3021d48bf21fecff825fdd9b91077e6c1
Abbas 01 <abbashere786@gmail.com>
2023-11-26 19:02:40 +0500

    Practicing condtionals with largerNumber problem

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const isEven = (num) => {
-    if (num % 2 === 0) {
-        return true;
-    } else if (num % 2 ==! 0) {
-        return false;
-    } else {
-        return "Numbers are only Valid characters";
-    }
-}
+// const largerNumber = (numOne, numTwo) => {
+//     if (numOne > numTwo) {
+//         return numOne;
+//     } else if (numOne < numTwo) {
+//         return numTwo;
+//     }
+// }
+// console.log(largerNumber(5, 20));
 
-console.log(isEven(2));
-console.log(isEven(5));
-console.log(isEven(1));
-console.log(isEven(10));
-console.log(isEven(34));
-console.log(isEven(100023004));
+const largerNumber = (numOne, numTwo) => {
+    return (numOne > numTwo) ? "numOne " + numOne : (numOne < numTwo) ? "numTwo " + numTwo : "Numbers are only valid characters" 
+} 
+console.log(largerNumber(1000, 10))
