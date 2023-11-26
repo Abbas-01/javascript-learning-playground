f9be7ab5b4ee23be4d7ec045301365afd919eec2
Abbas 01 <abbashere786@gmail.com>
2023-11-26 18:55:32 +0500

    Practicing Conditionals with isEven problem

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-class person {
-    constructor(fName, lName, age) {
-        this.firstName = fName;
-        this.lastName = lName;
-        this.age = age;
-    }
-    fullName () {
-        return `${this.firstName} ${this.lastName}`
+const isEven = (num) => {
+    if (num % 2 === 0) {
+        return true;
+    } else if (num % 2 ==! 0) {
+        return false;
+    } else {
+        return "Numbers are only Valid characters";
     }
 }
 
-let abbas = new person("Abbas", "Ali", 15);
-console.log(abbas.fullName())
+console.log(isEven(2));
+console.log(isEven(5));
+console.log(isEven(1));
+console.log(isEven(10));
+console.log(isEven(34));
+console.log(isEven(100023004));
