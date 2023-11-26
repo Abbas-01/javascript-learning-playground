3aca745de0740f7b345a64325b5fa70f80f1fdee
Abbas 01 <abbashere786@gmail.com>
2023-11-26 19:25:02 +0500

    Practicing conditionals with  Number Checker postive, negative, and zero

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const isVowel = (alpha) => {
-    let vowels = ["a", "e", "i", "o", "u"];
-    for (let i = 0; i < vowels.length; i++) {
-        if (vowels[i] === alpha) {
-            return true;
-        }
+const numChecker = (num) => {
+    if (num > 0) {
+        return "Positive";
+    } else if (num === 0) {
+        return "Zero";
+    } else if (num < 0) {
+        return "Negative";
     }
-    return false;
 }
-console.log(isVowel("q"));
-console.log(isVowel("e"));
-console.log(isVowel("z"));
-console.log(isVowel("a"));
-console.log(isVowel("y"));
-console.log(isVowel("x"));
+console.log(numChecker(5));
+console.log(numChecker(-2));
+console.log(numChecker(0));
+
+
+
+const numCheckerTernary = (num) => {
+    return (num > 0) ? "Positive" : (num === 0) ? "Zero" : (num < 0) ? "Negative" : "Numbers are only valid character";
+};
+
+console.log(numCheckerTernary(-1))
+console.log(numCheckerTernary(1))
+console.log(numCheckerTernary(0))
