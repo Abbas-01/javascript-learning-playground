a961a88c66d9cfb09d099551fc44431370d8e1d0
Abbas 01 <abbashere786@gmail.com>
2023-11-28 15:33:40 +0500

    Practicing arrays with return even array problem

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const findMinMax = (arr) => {
-    let min = arr[0];
-    let max = arr[0];
-
-    for (let i = 1; i < arr.length; i++) {
-        if(arr[i] < min) {
-            min = arr[i];
-        }
-        if (arr[i] > max) {
-            max = arr[i];
+    let evenArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            evenArr.push(arr[i]);
         }
     }
-
-
-    return [min, max];
+    return evenArr;
 
 }
 
-let x = [19, 43, 51, 69, 77, 92, 1];
+let x = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10];
 console.log(findMinMax(x));
