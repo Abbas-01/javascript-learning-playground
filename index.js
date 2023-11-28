8356498162f1c5eb411b5a8519dfeb7a8c2e802f
Abbas 01 <abbashere786@gmail.com>
2023-11-28 17:29:12 +0500

    Practicing arrays with remove duplicate elements of array problem

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let a = [1, 2, 5, 3, 1, 2];
-const mySet = new Set(a);
-
-mySet.forEach(x => {
-    console.log("Set element " + x);
-})
+const remDuplicate = (arr) => {
+    const removeDup = new Set(arr);
+    const nonDuplicate = Array.from(removeDup);
+    return nonDuplicate;
+}
+let a = [1, 2, 5, 2, 1, 34, 51]
+console.log(remDuplicate(a))
