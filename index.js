0770a1cb6d6a39a91a4862814280a44c77f6b5d8
Abbas 01 <abbashere786@gmail.com>
2023-11-28 17:40:34 +0500

    Practicng arrays with returning non duplicate element's array problem

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const remDuplicate = (arr) => {
-    const removeDup = new Set(arr);
-    const nonDuplicate = Array.from(removeDup);
+    let nonDuplicate = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr.includes(arr[i], i + 1)) {
+            nonDuplicate.push(arr[i]);
+        }
+    }
     return nonDuplicate;
 }
 let a = [1, 2, 5, 2, 1, 34, 51]
-console.log(remDuplicate(a))
+console.log(remDuplicate(a));
