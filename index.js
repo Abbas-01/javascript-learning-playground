f40bf4c07c03253788b67d740e253fcc0b5a559a
Abbas 01 <abbashere786@gmail.com>
2023-11-28 15:26:16 +0500

    Practicing arrays with max min elements of array

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const findMinMax = (arr) => {
-    let max = Math.max(...arr);
-    let min = Math.min(...arr);
+    let min = arr[0];
+    let max = arr[0];
 
-    return [max, min]
+    for (let i = 1; i < arr.length; i++) {
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+
+    return [min, max];
 
 }
 
-let x = [5, 100, 10000, 34, 0];
+let x = [19, 43, 51, 69, 77, 92, 1];
 console.log(findMinMax(x));
