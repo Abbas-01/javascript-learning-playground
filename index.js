53a16e4c1b1d24e920f9df078f8cb28bc943e8e2
Abbas 01 <abbashere786@gmail.com>
2023-11-29 12:38:42 +0500

    Doing the last problem with a easier method

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const rotateArrLeft = (arr, steps) => {
     steps = steps % arr.length;
-    const lastElements = arr.slice(steps - arr.length);
-    const rotatedArr = lastElements.concat(arr.slice(0, steps));
+    const rotatedArr = arr.slice(steps).concat(arr.slice(0, steps));
     return rotatedArr;
 };
 
 let x = [1, 2, 3, 4, 5, 6];
-console.log(rotateArrLeft(x, 10));
+console.log(rotateArrLeft(x, 4));
 
