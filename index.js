c819d247a8ae498c110b5360cc607d724a368ec6
Abbas 01 <abbashere786@gmail.com>
2023-11-30 16:16:46 +0500

    Practicing arrays with return a chunking array problem using slice method

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const chunkedArray = (arr, chunkSize) => {
-    let chunckArr = [];
-    let chunkedArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        chunckArr.push(arr[i]);  
+const chunkArr = (arr, chunkSize) => {
+    const result = [];
 
-        if (chunckArr.length === chunkSize || i === arr.length - 1) {
-            chunkedArr.push([...chunckArr]);
-            chunckArr = [];
-        }
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        result.push(arr.slice(i, i + chunkSize));
     }
-    return chunkedArr;
+    return result;
 }
 
-let x = ["Abbas", "Ali", "Asad", "adas", "dfdfg", "dfaf", "asdfhasufs"];
-let a = chunkedArray(x, 3);
-let y = [1, 2, 3, 4, 5, 6, 7];
-let b = chunkedArray(y, 4);
-console.log(a);
-console.log(b);
+console.log(chunkArr([1, 2, 3, 4, 5], 2));
