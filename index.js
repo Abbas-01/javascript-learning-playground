807bbc8ab6d08ad4a5e6dbe3ddde30d3bd82beda
Abbas 01 <abbashere786@gmail.com>
2023-11-30 23:40:20 +0500

    uniting two arrays

--- index.js
+++ index.js
@@ -1,46 +1,13 @@
-let array = [
-    {
-        marks: 20,
-        name: "Abbas",
-        age: 15
-    }, {
-        marks: 10,
-        name: "Asad",
-        age: 16
-    }, {
-        marks: 41,
-        name: "Farzain",
-        age: 16
-    }, {
-        marks: 23,
-        name: "Umair",
-        age: 16
-    }, {
-        marks: 49,
-        name: "Hanzala",
-        age: 16
-    }, {
-        marks: 36,
-        name: "Khizar",
-        age: 16
-    }
-];
+const names = ["Abbas", "Ali", "Asad"];
+const cities = ["Matli", "Hyderabad", "Lahore"];
 
-const result = (data) => {
-    for (let i = 0; i < data.length; i++) {
-        for (let j = i + 1; j < data.length; j++) {
-            if (data[i].marks < data[j].marks) {
-                let x = data[i];
-                data[i] = data[j];
-                data[j] = x;
-            }
-        }
-    }
-
-    for (let i = 0; i < data.length; i++) {
-        data[i].rank = i + 1;
-    }
+const both = [];
+const helo = (namesa, citiesa) =>{
+for (let i = 0; i < namesa.length; i++) {
+    both.push([]);
+    both[i].push(namesa[i]);
+    both[i].push(citiesa[i]);
 }
-
-result(array);
-console.log(array[2].rank)
+return both;
+}
+console.log(helo(names, cities))
