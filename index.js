036da08afe12421150b8cb1acafb7fe4f8c93335
Abbas 01 <abbashere786@gmail.com>
2023-11-30 23:14:44 +0500

    Practicing with sorting data problem

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
-const chunkArr = (arr, chunkSize) => {
-    const result = [];
-
-    for (let i = 0; i < arr.length; i += chunkSize) {
-        result.push(arr.slice(i, i + chunkSize));
+let array = [
+    {
+        marks: 20,
+        name: "Abbas",
+        age: 15
+    }, {
+        marks: 10,
+        name: "Asad",
+        age: 16
+    }, {
+        marks: 41,
+        name: "Farzain",
+        age: 16
+    }, {
+        marks: 23,
+        name: "Umair",
+        age: 16
+    }, {
+        marks: 49,
+        name: "Hanzala",
+        age: 16
+    }, {
+        marks: 36,
+        name: "Khizar",
+        age: 16
     }
-    return result;
+];
+
+const result = (data) => {
+    for (let i = 0; i < data.length; i++) {
+        for (let j = i + 1; j < data.length; j++) {
+            if (data[i].marks < data[j].marks) {
+                let x = data[i];
+                data[i] = data[j];
+                data[j] = x;
+            }
+        }
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        data[i].rank = i + 1;
+    }
 }
 
-console.log(chunkArr([1, 2, 3, 4, 5], 2));
+result(array);
+console.log(array[2].rank)
