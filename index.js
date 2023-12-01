4321a824d8ad8569ebc916d56e052390b07c0e78
Abbas 01 <abbashere786@gmail.com>
2023-12-01 23:43:38 +0500

    Practicing closures problem three

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-// Create a function that returns a counter function
-function createCounter() {
-  let count = 0;
+// Modify the following code to use closures and make the 'balance' variable private
+function createAccount(initialBalance) {
+  let balance = initialBalance;
 
-  // Your code here: Return a function that increments and returns the count
-  function countIncrement () {
-    count++;
-    return count;
-  }
-  return countIncrement;
+  return {
+    deposit: function(amount) {
+      // Your code here: Add amount to the balance
+      balance += amount;
+      console.log(`Deposite Amount is ${amount}. New Balance is ${balance}`);
+    },
+    withdraw: function(amount) {
+        // Your code here: Subtract amount from the balance if sufficient funds
+        if (balance >= amount) {
+            balance -= amount;
+            console.log(`Withdrawn Amount is ${amount}. New Balance is ${balance}`);
+        } else {
+            console.log(`Insufficient funds. Withdrawl cancel`);
+        }
+    },
+    getBalance: function() {
+      // Your code here: Return the current balance
+      console.log(`Current Balance is ${balance}`)
+      return balance;
+    }
+  };
 }
 
-// Create two counters using the createCounter function
-let counter1 = createCounter();
-let counter2 = createCounter();
+// Create an account with an initial balance of 100
+let account = createAccount(100);
 
-// Use the counters to increment and log values
-console.log(counter1())
-console.log(counter1())
-console.log(counter2())
-console.log(counter2())
-// Expected Output:
-// 1
-// 2
-// 1
-// 2
+// Use the account methods to deposit, withdraw, and get balance
+account.getBalance()
+account.deposit(100)
+account.withdraw(20)
+account.getBalance()
