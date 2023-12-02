4378aab4dcd573631a132bee149f5533bbb2570b
Abbas 01 <abbashere786@gmail.com>
2023-12-02 17:00:40 +0500

    Practicing higher order function with custommap problem

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const greet = (name = "Stranger") => {
-  return `Hello! ${name}`
+function customMap(arr, callback) {
+  // Your code here
+  let x = [];
+  for (let i = 0; i < arr.length; i++) {
+    x.push(callback(arr[i]));
+  }
+  return x;
 }
-const helo = (callBack, person) => {
-  return callBack(person);
-}
-// console.log(helo(greet, "Abbas"));
-console.log(helo(greet, "Abbas"))
 
+// Example usage:
+let numbers = [1, 2, 3, 4, 5];
+let squaredNumbers = customMap(numbers, function(num) {
+  return num * num;
+});
+console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
