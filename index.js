183d99c9d5561353192a48b1b4a3160f026983c7
Abbas 01 <abbashere786@gmail.com>
2023-12-03 16:52:01 +0500

    Practicing Recursion

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-(function (num) {
-    console.log(num * num);
-}) (3);
+const recursiveFunc = (num) => {
+    if (num === 10) { // Base Condition
+        console.log(num);
+        return;
+    }
+
+    console.log(num);
+    recursiveFunc(num + 1); // Recursive Call
+}
+
+recursiveFunc(2);
