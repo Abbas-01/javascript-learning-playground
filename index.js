56d3ab10c2e8606d92169868d6c5752cd0d09385
Abbas 01 <abbashere786@gmail.com>
2023-12-05 17:36:45 +0500

    Practicing forEach with updating elements problem

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-let x = [7, 4, 2, 9, 4];
+let stringsArr = ["ElementOne", "ElementTwo", "ElementThree", "ElementFour"];
 
-let sumOfElements = 0;
-let count = 0;
-x.forEach((a) => {
-    sumOfElements += a;
-    count++;
-});
-
-if (count === 0) {
-    console.log(0);
-} else {
-    console.log(Math.round(sumOfElements / count));
-}
-
-
+stringsArr.forEach((a, i, arr) => {
+    arr[i] = `${a} Updated`
+})
+console.log(stringsArr)
