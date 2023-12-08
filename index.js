2632c43e2c3e0a3bf1cf61c861e874a9903f120f
Abbas 01 <abbashere786@gmail.com>
2023-12-08 17:44:22 +0500

    Practicing filter method by returning a array of odd Numbers

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const squareRoot = (num) => {
-    for (let i = 1; i < num; i++) {
-        if (num / i === i) {
-            return i;
-        }
-    }
-}
-
-const squareRootsArr = (arr) => {
-    return arr.map(squareRoot);
-}
-
-const x = [4, 9, 16, 25, 36, 49];
-console.log(squareRootsArr(x));
+const filterOddNums = (arr) => {
+    return arr.filter(num => {
+        return num % 2 !== 0;
+    })
+} 
+console.log(filterOddNums([1, 2, 3, 4, 5]));
