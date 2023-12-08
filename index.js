5f8cbaa57873caa6f59f01709df216785ba632be
Abbas 01 <abbashere786@gmail.com>
2023-12-08 18:12:36 +0500

    Practicing filter by filtering books from books object

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const positiveNums = (arr) => {
-    return arr.filter(num => {
-        return num > 0;
-    })
-} 
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
 
-const numbersArr = [32, -6, -1, -11, 7, 22];
-console.log(positiveNums(numbersArr));
+const userBooks = (Genre) => {
+    return books.filter((book) => {
+        return book.genre === Genre;
+    });
+};
+console.log(userBooks("Non-Fiction"))
