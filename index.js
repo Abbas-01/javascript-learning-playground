43e070acb6709cd1eb1cb528a1757a8dd701b880
Abbas 01 <abbashere786@gmail.com>
2023-12-09 16:06:10 +0500

    Practicing filter method by returning a array of only palindrome words

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const books = [
-    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
-    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
-    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
-    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
-    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
-    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
-    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
-    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
-    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
-  ];
+    const isPalindrome = (str) => {
+        for (let i = 0; i < str.length / 2; i++) {  
+            if(str[i] !== str[str.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    const helo = (arr) => {
+        return arr.filter(item => {
+            if (isPalindrome(item.toLowerCase())) {
+                return item;
+            }
+        });
+    }
 
-const userBooks = (Genre, publishDate) => {
-    return books.filter((book) => {
-        return book.genre === Genre && book.publish >= publishDate;
-    });
-};
-console.log(userBooks("Non-Fiction", 1980))
+    const x = ["AbsbA", "civic", "Nissan", "amma"];
+    console.log(helo(x));
