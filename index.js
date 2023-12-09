bf79995b9ea5185cb027a3b229666113ee8047fb
Abbas 01 <abbashere786@gmail.com>
2023-12-09 19:40:44 +0500

    using single line block

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const remDuplicate = (arr) => {
-    return arr.filter((num, i, arr) => {
-        return (!arr.includes(num, i + 1))
-    })
+    return arr.filter((num, i, arr) => (!arr.includes(num, i + 1)))
 } 
 console.log(remDuplicate([1, 2, 4, ,55,1 , 2]));
