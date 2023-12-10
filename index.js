14cdf1d2ce5d1902cec2e715d95edfa9f479a0e3
Abbas 01 <abbashere786@gmail.com>
2023-12-10 13:17:54 +0500

    Practicing reduce method by returning total sum of the items prices

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const totalSum = (arr) => {
-    return arr.reduce((acc, currVal) => acc+currVal, 0);
-}
 
-console.log(totalSum([1, 2, 3, 4, 5]));
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const totalPrice = (arr) => {
+    return arr.reduce((acc, currVal) => acc+currVal.price, 0);
+}
+console.log(totalPrice(shoppingCart));
