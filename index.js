8e173013a982f3ae633755a1359c24f941ed8396
Abbas 01 <abbashere786@gmail.com>
2023-12-10 13:11:21 +0500

    Practicing reduce method by returning total sum of numbers in an array

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
-const filterStatus = (userStatus) => {
-    return data.filter(item => {
-        return item.status === userStatus;
-    })
-};
-const data = [
-    {
-        name: "Abbas",
-        age: 15,
-        status: "Focused"
-    },{
-        name: "Ali",
-        age: 19,
-        status: "Chill"
-    },{
-        name: "Tanveer",
-        age: 24,
-        status: "Focused"
-    },{
-        name: "Asad",
-        age: 16,
-        status: "Chill"
-    },{
-        name: "Farzain",
-        age: 16,
-        status: "Chill"
-    }, 
-];
-console.log(filterStatus("Focused"));
+const totalSum = (arr) => {
+    return arr.reduce((acc, currVal) => acc+currVal, 0);
+}
+
+console.log(totalSum([1, 2, 3, 4, 5]));
