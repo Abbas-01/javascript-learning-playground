4d8bdfddee4a29b407983db690ae430b9a5b7675
Abbas 01 <abbashere786@gmail.com>
2023-12-11 21:06:41 +0500

    Practicing reduce method with grouping objects by properties

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
-const data = [
-    {
-        userName: "Abbas",
-        password: "aod29na0",
-        age: 16
-    }, {
-        userName: "Tanveer",
-        password: "a1312ca0",
-        age: 24
-    },{
-        userName: "Farzain",
-        password: "adb28a0h",
-        age: 16
-    },{
-        userName: "Ali",
-        password: "5623znaa2",
-        age: 19
-    },{
-        userName: "Asad",
-        password: "219anghg9",
-        age: 16
-    },{
-        userName: "Hanzala",
-        password: "12n9xa1a",
-        age: 16
-    },
-];
-
-const groupByProperty = (arr) => {
-    return arr.reduce((acc, currVal) => {
-        if (currVal.age > 18) {
-            acc.adult.push(currVal);
-        } else {
-            acc.teen.push(currVal);
-        }
-        return acc;
-    }, {adult: [],teen: []});
-};
-
-console.log(groupByProperty(data));
+const groupByElements = (arr, prop) => {
+  return arr.reduce((acc, currVal) => {
+    let key = currVal[prop];
+    if (!acc[key]) {
+      acc[key] = []
+    }
+    acc[key].push(currVal);
+    return acc
+  }, {})
+}
+  
+  const data = [
+    { userName: "Abbas", password: "aod29na0", status: "A" },
+    { userName: "Tanveer", password: "a1312ca0", status: "B" },
+    { userName: "Farzain", password: "adb28a0h", status: "A" },
+    { userName: "Ali", password: "5623znaa2", status: "C" },
+    { userName: "Asad", password: "219anghg9", status: "B" },
+    { userName: "Hanzala", password: "12n9xa1a", status: "A" },
+    { userName: "Asad", password: "219anghg9", status: "C" },
+    { userName: "Hanzala", password: "12n9xa1a", status: "A" },
+  ];
+  
+  console.log(groupByElements(data, "status"));
+  
