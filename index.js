fe157bf469663a30a97ecdb135323fc87b8ec6a8
Abbas 01 <abbashere786@gmail.com>
2023-12-11 21:30:26 +0500

    Practicing reduce method by reversing a string

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const groupByElements = (arr, prop) => {
-  return arr.reduce((acc, currVal) => {
-    let key = currVal[prop];
-    if (!acc[key]) {
-      acc[key] = []
-    }
-    acc[key].push(currVal);
-    return acc
-  }, {})
+const revString = (str) => {
+  return str.split("")  .reduce((acc, currVal) => {
+    return currVal + acc  
+  }, " ")
 }
-  
-  const data = [
-    { userName: "Abbas", password: "aod29na0", status: "A" },
-    { userName: "Tanveer", password: "a1312ca0", status: "B" },
-    { userName: "Farzain", password: "adb28a0h", status: "A" },
-    { userName: "Ali", password: "5623znaa2", status: "C" },
-    { userName: "Asad", password: "219anghg9", status: "B" },
-    { userName: "Hanzala", password: "12n9xa1a", status: "A" },
-    { userName: "Asad", password: "219anghg9", status: "C" },
-    { userName: "Hanzala", password: "12n9xa1a", status: "A" },
-  ];
-  
-  console.log(groupByElements(data, "status"));
-  
+
+console.log(revString("Abbas"));
