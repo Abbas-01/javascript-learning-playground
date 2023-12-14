17fe031fe900f6fa9e7ea6f43a65d796ae638f50
Abbas 01 <abbashere786@gmail.com>
2023-12-14 13:12:22 +0500

    Practicing Object.create method with inheritance chain problem

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const carsPrototype = {
-    model: "Ford GT",
-    color: "Red",
-    doors: 2,
-    engine () {
-        return "V6";
-    },
-    sound () {
-        return "Vrooom!"
+const vehilce = {
+    company: "Toyota",
+    doors: 4,
+    drive () {
+        return `You are driving a ${this.company} vehicle`
     }
-};
+}
 
-const audi = Object.create(carsPrototype);
-console.log(audi.sound())
+const car = Object.create(vehilce);
+car.model = "Corolla";
+car.drive = function () {return `You are driving a ${this.company} ${this.model}`};
+
+const sportCar = Object.create(car);
+sportCar.model = "Supra";
+sportCar.engine = "V6"
+sportCar.doors = 2;
+
+console.log(sportCar);
+console.log(car);
+console.log(vehilce);
