07c79c87c6267d642ba0bfbdf40edb0a44dc6efc
Abbas 01 <abbashere786@gmail.com>
2023-12-16 16:40:22 +0500

    Accessing h1 element using Javascript

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-const vehilce = {
-    company: "Toyota",
-    doors: 4,
-    drive () {
-        return `You are driving a ${this.company} vehicle`
-    }
-}
-
-const car = Object.create(vehilce);
-car.model = "Corolla";
-car.drive = function () {return `You are driving a ${this.company} ${this.model}`};
-
-const sportCar = Object.create(car);
-sportCar.model = "Supra";
-sportCar.engine = "V6"
-sportCar.doors = 2;
-
-console.log(sportCar);
-console.log(car);
-console.log(vehilce);
+const h1 = document.getElementById("javascriptHeading");
+console.log(h1);
