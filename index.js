c10929d5c39ab4562875bebe7041deda184a16c8
Abbas 01 <abbashere786@gmail.com>
2023-12-18 16:20:49 +0500

    Removing an HTML element using remove() method

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-const paragraph = document.createElement("p");
-paragraph.setAttribute("class", "paragraph");
-paragraph.textContent = "This is a paragraph";
-document.body.appendChild(paragraph);
+const firstParagraph = document.querySelector(".paragraph")
+firstParagraph.remove()
