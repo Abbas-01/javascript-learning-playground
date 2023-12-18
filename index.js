300acdb555d8b8ad38e35cfb6ad372c3632cc62b
Abbas 01 <abbashere786@gmail.com>
2023-12-18 18:21:41 +0500

    Creating a small project Color Scheme Switcher

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const firstParagraph = document.querySelector("p:nth-Child(1)")
-const subHeading = document.createElement("h2");
-subHeading.appendChild(document.createTextNode("About"));
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll("li");
 
-firstParagraph.replaceWith(subHeading);
+buttons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        if (e.target.id === "gray") {
+                body.style.backgroundColor = "rgb(177, 177, 177)";
+        } else if (e.target.id === "red") {
+            body.style.backgroundColor = "rgb(255, 73, 73)";
+        } else if (e.target.id === "yellow") {
+            body.style.backgroundColor = "rgb(240, 240, 94)";
+        } else if (e.target.id === "blue") {
+            body.style.backgroundColor = "rgb(66, 66, 255)";
+        }
+    })
+});
