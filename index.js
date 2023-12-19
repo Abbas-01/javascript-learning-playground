2e34ab639c6ee112eacc2e1a40dec093229e1765
Abbas 01 <abbashere786@gmail.com>
2023-12-19 15:27:47 +0500

    Created a BMI Calculator Project

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const body = document.querySelector("body");
-const buttons = document.querySelectorAll("li");
+const form = document.querySelector("form");
+form.addEventListener("submit" , (e) => {
+    e.preventDefault();
+    const weight = parseInt(document.querySelector("#weightInput").value)
+    const height = parseInt(document.querySelector("#heightInput").value)
+    const result = document.querySelector("#result");
+    let resultValue;
+    if (weight === "" || weight < 0 || isNaN(weight)) {
+         result.innerHTML = `Please give us a valid weight ${weight} `
+    } else if (height === "" || height < 0 || isNaN(height)) {
+        result.innerHTML = `Please give us a valid height ${height} `
+    } else {       
+        resultValue = (weight / ((height * height) / 10000)).toFixed(1)
+    }
+    if (resultValue >= 25.0) {
+        result.innerHTML = `Your Body Mass Index is ${(weight / ((height * height) / 10000)).toFixed(1)}. This is Considered overweight`
+    } else if (resultValue <= 18.4) {
+        result.innerHTML = `Your Body Mass Index is ${(weight / ((height * height) / 10000)).toFixed(1)}. This is Considered underweight`
+    } else if (resultValue < 25.0 && resultValue > 18.4) {
+        result.innerHTML = `Your Body Mass Index is ${(weight / ((height * height) / 10000)).toFixed(1)}. This is Considered normal`
+    }
+})
 
-buttons.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        if (e.target.id === "gray") {
-                body.style.backgroundColor = "rgb(177, 177, 177)";
-        } else if (e.target.id === "red") {
-            body.style.backgroundColor = "rgb(255, 73, 73)";
-        } else if (e.target.id === "yellow") {
-            body.style.backgroundColor = "rgb(240, 240, 94)";
-        } else if (e.target.id === "blue") {
-            body.style.backgroundColor = "rgb(66, 66, 255)";
-        }
-    })
-});
