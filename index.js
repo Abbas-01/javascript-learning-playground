a0d3e40c29d5d64c9b66a1cfbbe3ae7de2c47ddd
Abbas 01 <abbashere786@gmail.com>
2023-12-23 21:34:11 +0500

    Learning event propagation

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const anchor = document.querySelector("a");
-anchor.addEventListener("click", (e) => {
-    e.preventDefault()
-})
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+const subChild = document.querySelector(".subChild");
+
+let count = 0;
+parent.addEventListener("click", (e) => {
+    console.log(count++)
+    console.log("Parent")
+}, true);
+child.addEventListener("click", (e) => {
+    console.log(count++)
+    console.log("Child")
+}, true);
+subChild.addEventListener("click", (e) => {
+    console.log(count++)
+    console.log("Sub Child")
+}, true);
