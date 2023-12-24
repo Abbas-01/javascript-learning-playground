15866b9dff9c819b47ca6b776e6376ff62cd45fc
Abbas 01 <abbashere786@gmail.com>
2023-12-24 15:00:08 +0500

    Learning event delegation

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const parent = document.querySelector(".parent");
-const child = document.querySelector(".child");
-const subChild = document.querySelector(".subChild");
 
 parent.addEventListener("click", (e) => {
-    console.log("Parent")
-}, true);
-child.addEventListener("click", (e) => {
-    console.log("Child")
-}, false);
-subChild.addEventListener("click", (e) => {
-    e.stopPropagation()
-    console.log("Sub Child")
-}, false);
+    if (e.target.matches("li")) {
+        console.log(`This is ${e.target.id}`);
+    }
+})
