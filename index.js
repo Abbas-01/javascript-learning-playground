5da8bfd7887979b084e387b2a492bc758b413815
Abbas 01 <abbashere786@gmail.com>
2023-12-24 21:48:50 +0500

    Created a sum calculator

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const parent = document.querySelector(".parent");
+const numInput = document.querySelector("#numberInput");
+const sumBtn = document.querySelector("#sumBtn");
+const resetBtn = document.querySelector("#resetBtn");
+const form = document.querySelector("form");
 
-parent.addEventListener("click", (e) => {
-    if (e.target.matches("li")) {
-        console.log(`This is ${e.target.id}`);
+let sumOfNum = 0;
+
+sumBtn.addEventListener("click", (e) => {
+    const inputNum = parseInt(numInput.value);
+    if (!isNaN(inputNum)) {
+        sumOfNum += inputNum;
+        numInput.value = "";
+    } else {
+        alert("Please give us a valid Number")
+        numInput.value = "";
     }
+})
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputNum = parseInt(numInput.value);
+    numInput.value = `${sumOfNum + inputNum}`;
+    sumOfNum = 0;
+})
+resetBtn.addEventListener("click", (e) => {
+    numInput.value = "";
+  sumOfNum = 0;  
 })
