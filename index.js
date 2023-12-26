0b500cd5813e8edf147e25d85bad07ce1b21761c
Abbas 01 <abbashere786@gmail.com>
2023-12-26 17:42:32 +0500

    Practicing setInterval by creating random backgroundColor changer

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+const body = document.querySelector("body");
 const startBtn = document.querySelector("#start");
 const stopBtn = document.querySelector("#stop");
 
-const sayDate = (myName) => {
-    console.log(myName + " " + Date.now());
-}
 let invalidID;
+function randomColor () {
+    let color = "";
+    for (let i = 0; i < 3; i++) {
+        color += ` ${Math.floor(Math.random() * 256)}` 
+    }
+    return body.style.backgroundColor = `rgb(${color})`
+}
+
 startBtn.addEventListener("click", () => {
-    invalidID = setInterval(sayDate, 1000, "Abbas");      
-});
+    if (!invalidID){
+        invalidID = setInterval(randomColor, 1000);
+    }
+})
 
 stopBtn.addEventListener("click", () => {
     clearInterval(invalidID);
-});
+    invalidID = null;
+})
