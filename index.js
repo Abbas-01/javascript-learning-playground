c54b86488b83f3cfb1ece710629c740ba189a45b
Abbas 01 <abbashere786@gmail.com>
2023-12-27 17:56:16 +0500

    Handling error using catch

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const myPromise = new Promise((resolve, reject) => {
     setTimeout(function () {
-        console.log("Async Task");
-        resolve({userName: "Abbas", age: 16})
+        let error = true;
+        if (!error) {
+            resolve({userName: "Abbas", age: 16})
+        } else {
+            reject("Error: There is an error")
+        }
     }, 1000)
 })
 
 myPromise
 .then(function(userData) {
-    console.log(userData)
+    console.log(userData.userName)
+}).catch(function(error){
+    console.log(error);
 })
