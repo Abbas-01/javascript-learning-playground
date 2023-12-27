f2bdc4e5a450ee80882b855611d9466d4be6251c
Abbas 01 <abbashere786@gmail.com>
2023-12-27 17:45:24 +0500

    Learning promises

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const personName = document.querySelector("#personName");
-const followers = document.querySelector("#followersCount");
-const userImg = document.querySelector("#image");
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(function () {
+        console.log("Async Task");
+        resolve()
+    }, 1000)
+})
 
-const xhr = new XMLHttpRequest();
-const requestURL = "https://api.github.com/users/hiteshchoudhary"; 
-xhr.open("GET", requestURL);
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        const data = JSON.parse(this.responseText);
-        userImg.setAttribute("src", `${data.avatar_url}`)
-        personName.innerHTML = `${data.name}`;
-        followers.innerHTML = `${data.followers}`;
-    }
-}
-xhr.send();   
+myPromise
+.then(function() {
+    console.log("Promise Resolved")
+})
