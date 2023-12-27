c81e6d4887361f56b3911f914acf2c423bc9b712
Abbas 01 <abbashere786@gmail.com>
2023-12-27 17:59:08 +0500

    Chaining then in promises

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const myPromise = new Promise((resolve, reject) => {
     setTimeout(function () {
-        let error = true;
+        let error = false;
         if (!error) {
             resolve({userName: "Abbas", age: 16})
+            console.log("Async Task")
         } else {
             reject("Error: There is an error")
         }
@@ -11,7 +12,12 @@ const myPromise = new Promise((resolve, reject) => {
 
 myPromise
 .then(function(userData) {
-    console.log(userData.userName)
-}).catch(function(error){
+    console.log(userData)
+    return userData.userName
+})
+.then(function(userName){
+    console.log(userName)
+})
+.catch(function(error){
     console.log(error);
 })
