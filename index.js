1bc88df8b967b5cd8bc761ad645734904f8f4016
Abbas 01 <abbashere786@gmail.com>
2024-01-08 20:46:56 +0500

    created a simple weather app

--- index.js
+++ index.js
@@ -1,87 +1,79 @@
-const valInput = document.querySelector("#valueInput");
-const btns = document.querySelectorAll("button");
-const btnsArr = Array.from(btns);
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
 
-let exp = "";
+const weatherDetailsContainer = document.querySelector(".weatherDetails");
+const otherDetails = document.querySelector(".otherDetails");
+const hideOrShow = document.querySelectorAll(".hideShow") 
+const cityInputField = document.querySelector("#cityInput");
+const searchBtn = document.querySelector("#searchBtn");
+const cityTempEle = document.querySelector(".cityTemp");
+const cityNameEle = document.querySelector(".cityName");
+const humidity = document.querySelector("#humidity");
+const windSpeed = document.querySelector("#windSpeed");
+const weatherImage = document.querySelector("#weatherImg");
 
-function handleNumberBtnClick (btn) {
-    const number = btn.innerHTML;
-    exp += number;
-    valInput.value += number;    
-};
-function handleSymbolBtnClick (btn) {
-    const sign = btn.innerHTML;
-    if (exp !== "" && (sign === "-" && exp[exp.length - 1] === "*" || exp.length === 1 || !isNaN(exp[exp.length - 1]))) {
-        exp += sign;
-        valInput.value  += sign;
-    } else {
-        return;
-    }
-};
-function handlePercentageBtnClick () {
-    if (exp !== "") {
-        let num = "";
-        for (let i = exp.length - 1; i >= 0; i--) {
-            if (!isNaN(exp[i])) {
-                num = exp[i] + num;
-            } else {
-                break;
-            }
-        }
-        const finalVal = parseFloat(num) / 100;
-        valInput.value += "%*";
-        exp = `${exp.substring(0, exp.length - num.length)}${finalVal}*`;
-    } else {
-        return;
-    }
-}   
-function handleEvaluateBtnClick () {
-    try {
-        const result = new Function("return " + exp)();
-        exp = result.toString();
-        valInput.value = exp;
-    } catch {
-        alert("Expression Error")
-    }
-};
-
-function handleDeleteBtnClick () {
-    if (valInput.value.length === 1) {
-        exp = "";
-    } 
-    if (valInput.value.length > 0) {
-        const expArr = exp.split("");
-        const inputArr = valInput.value.split("");
-        expArr.pop();
-        inputArr.pop();
-        exp = expArr.join("");
-        valInput.value = inputArr.join("");        
-        console.log(exp)
-    }
-};
-
-function handleResetBtnClick () {
-    exp = "";
-    valInput.value = "";
-};
-
-btnsArr.map(btn => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        if(btn.classList.contains("num")) {
-            handleNumberBtnClick(btn);
-        } else if (btn.classList.contains("symbol")) {
-            handleSymbolBtnClick(btn);
-        } else if (btn.classList.contains("percentageBtn")) {
-            handlePercentageBtnClick();
-        } else if (btn.classList.contains("evaluateBtn")) {
-            handleEvaluateBtnClick();      
-        } else if (btn.classList.contains("delBtn")) {
-            handleDeleteBtnClick();
-        } else if (btn.classList.contains("resBtn")) {
-            handleResetBtnClick();
-        }
+function resetingElements () {
+    otherDetails.style.display = "none";
+    hideOrShow.forEach(ele => {
+        ele.style.display = "none";
     })
-});
+}
+function displayingWeather (data) {
+    cityTempEle.innerHTML = `${Math.round(data.main.temp)} <span class="dot">&#8728;</span>C`;
+        cityNameEle.innerHTML = `${data.name}`;
+        humidity.innerHTML = `${data.main.humidity} %`
+        windSpeed.innerHTML = `${Math.round((data.wind.speed * 3600) / 1000)} Km/h`
+        switch (data.weather[0].main) {
+            case "Clouds": 
+            weatherImage.setAttribute("src", "/img/clouds.png");
+            break;
+            case "Mist" :
+                weatherImage.setAttribute("src", "/img/mist.png");
+            break;
+            case "Snow" :
+                weatherImage.setAttribute("src", "/img/snow.png");
+            break;
+            case "Rain" :
+                weatherImage.setAttribute("src", "/img/rain.png");
+            break;
+            case "Clear" :
+                weatherImage.setAttribute("src", "/img/sun.png");
+            break;
+            case "Haze" :
+                weatherImage.setAttribute("src", "/img/haze.png");
+            break;
+            default :
+                weatherImage.setAttribute("src", "/img/clear-sky.png");
+            break;
+        }
+}
 
-console.log((1 + 2) / 5)
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let cityName = cityInputField.value;
+    cityName = cityName.replaceAll(" ", '')
+    const firstNumIndex = cityName.split("").findIndex(char => !isNaN(Number(char)));
+    resetingElements()
+    if (cityName === "") {
+        alert("please give us a name first")
+    } else if (firstNumIndex >= 0) {
+        alert("Please give us valid name");
+    } else {
+        weatherDetailsContainer.style.display = "block";
+        gettingWeather(cityName);   
+    }
+})
+async function gettingWeather(cityName) {
+    const response = await fetch(`${baseUrl}q=${cityName}&appid=20bce2cb30ef0934691dcac34c7f8f0c&units=metric`);
+    if (response.ok === false || response.status !== 200) {
+        hideOrShow[0].style.display = "block"
+        hideOrShow[0].innerHTML = `Sorry we are unable to get the weather details of "${cityName}"`;
+        weatherImage.setAttribute("src", "/img/33.png");
+        cityInputField.value = "";
+    } else {
+        hideOrShow[1].style.display ="block";
+        hideOrShow[2].style.display ="block";
+        otherDetails.style.display = "flex";
+        const data = await response.json();
+        displayingWeather(data);
+    }
+}
