203d529bbeb105e51ef24c05a0dd0cfad07fc13b
Abbas 01 <abbashere786@gmail.com>
2024-01-13 16:18:25 +0500

    Improved code by removing arrays and using classes instead to check winner

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const crossScore = document.querySelector("#crossScore");
 const zeroScore = document.querySelector("#zeroScore");
 const restartBtn = document.querySelector("#restartBtn");
 const winMessage = document.querySelector("#winMessage");
-console.log(zeroScore)
+
+
 let currSign;
 let numOfSlots = 9;
-let zeroArr = [];
-let crossArr = [];
-
 function showSignSeclecingSec() {
     document.querySelector(".signSelectingSec").style.display = "flex";
     boxesContainer.style.display = "none";    
@@ -23,64 +21,62 @@ function hideSignSeclecingSec() {
 
 signsContainer.addEventListener("click", (e) => {
     if (e.target.id === "cross") {
-        currSign = e.target.id;
+        currSign = "X";
         hideSignSeclecingSec()        
     } else if (e.target.id === "zero") {
-        currSign = e.target.id;
+        currSign = "O";
         hideSignSeclecingSec()
     }
 })
 
 function restartGame () {
     currSign = "";
-    zeroArr = [];
-    crossArr = []
     numOfSlots = 9;
     boxes.forEach(box => {
         box.innerHTML = "";
         box.setAttribute("isClicked", "false");
+        box.classList.remove("X");
+        box.classList.remove("O");
     });
     restartBtn.parentElement.style.display = "none";
     showSignSeclecingSec();
 }
-function checkWinner (arr) {
-    const methods = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];            
 
-    for (let method of methods) {
-        if (method.every(slot => arr.includes(slot))) {
+function checkWinner (sign) {
+    const winingCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];            
+    for (let combination of winingCombinations) {
+        if (combination.every(index => boxes[index].classList.contains(sign))) {
             return true;
         }
     }
     return false;
 }
 
-function displayingSigns (e, num) {
+function displayingSigns (e) {
     numOfSlots--;
-    if (currSign === "cross") {
+    if (currSign === "X") {
         e.target.innerHTML = `<div class="cross"></div>`;
-        crossArr.push(num);
-        currSign = "zero";
-        if (crossArr.length > 2 && checkWinner(crossArr)) {; 
+        e.target.classList.add(currSign);
+
+        if (checkWinner(currSign)) {    
             crossScore.textContent = `${parseInt(crossScore.textContent) + 1}`;
         }
-    } else if (currSign === "zero") {
+    } else if (currSign === "O") {
         e.target.innerHTML = `<div class="circle"></div>`;
-        zeroArr.push(num);
-        currSign = "cross";
-        if (zeroArr.length > 2 && checkWinner(zeroArr)) {
+        e.target.classList.add(currSign);
+
+        if (checkWinner(currSign)) {
             zeroScore.textContent = `${parseInt(zeroScore.textContent) + 1}`;
         }
     }
-    if (checkWinner(crossArr))  {
+    if (checkWinner(currSign))  {
         restartBtn.parentElement.style.display = "block";
-        winMessage.innerHTML = `X Wins`;
-    } else if (checkWinner(zeroArr)) {
-        restartBtn.parentElement.style.display = "block";
-        winMessage.innerHTML = `O Wins`;
+        winMessage.innerHTML = `${currSign} Wins`;
     } else if (numOfSlots === 0) {
         restartBtn.parentElement.style.display = "block";
         winMessage.innerHTML = `Draw`;
     } 
+    currSign = (currSign === "O") ? "X" : "O"; 
 }
 restartBtn.addEventListener("click", restartGame);
 
@@ -88,8 +84,7 @@ boxesContainer.addEventListener("click", (e) => {
     if (currSign) {
         let isClicked = e.target.getAttribute("isClicked");
         if (isClicked === "false") {
-            const boxNum = parseInt(e.target.id.replace("box", ""));
-            displayingSigns(e, boxNum);
+            displayingSigns(e);
             e.target.setAttribute("isClicked", "true");
         } 
     }
