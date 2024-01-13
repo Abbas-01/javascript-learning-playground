8f7ff92ef2300aa7d91b4c0590682a5c491463c1
Abbas 01 <abbashere786@gmail.com>
2024-01-13 13:54:32 +0500

    Created Tic Tac Toe game

--- index.js
+++ index.js
@@ -1,61 +1,96 @@
-const baseUrl = "https://api.openweathermap.org/data/2.5/";
+const boxesContainer = document.querySelector(".boxesWrapper");
+const signsContainer = document.querySelector(".signContainer");
+const boxes = document.querySelectorAll(".box");
+const crossScore = document.querySelector("#crossScore");
+const zeroScore = document.querySelector("#zeroScore");
+const restartBtn = document.querySelector("#restartBtn");
+const winMessage = document.querySelector("#winMessage");
+console.log(zeroScore)
+let currSign;
+let numOfSlots = 9;
+let zeroArr = [];
+let crossArr = [];
 
-const weatherDetailsContainer = document.querySelector(".weatherDetails");
-const otherDetails = document.querySelector(".otherDetails");
-const hideOrShow = document.querySelectorAll(".hideShow") 
-const cityInputField = document.querySelector("#cityInput");
-const searchBtn = document.querySelector("#searchBtn");
-const cityTempEle = document.querySelector(".cityTemp");
-const cityNameEle = document.querySelector(".cityName");
-const cityWeatherEle = document.querySelector(".cityWeather");
-const humidity = document.querySelector("#humidity");
-const windSpeed = document.querySelector("#windSpeed");
-const weatherImage = document.querySelector("#weatherImg");
-const cities = document.querySelectorAll(".cityNames");
+function showSignSeclecingSec() {
+    document.querySelector(".signSelectingSec").style.display = "flex";
+    boxesContainer.style.display = "none";    
+}
 
+function hideSignSeclecingSec() {
+    document.querySelector(".signSelectingSec").style.display = "none";
+    boxesContainer.style.display = "flex";    
+}
 
-function validatingCityName (cityName) {
-    const firstNumIndex = cityName.split("").findIndex(char => !isNaN(Number(char)));
-    if (cityName === "") {
-        alert("please give us a name first")
-    } else if (firstNumIndex >= 0) {
-        alert("Please give us valid name");
-    } else {    
-        gettingCurrentWeather(cityName);   
-        
+signsContainer.addEventListener("click", (e) => {
+    if (e.target.id === "cross") {
+        currSign = e.target.id;
+        hideSignSeclecingSec()        
+    } else if (e.target.id === "zero") {
+        currSign = e.target.id;
+        hideSignSeclecingSec()
     }
-}
-
-function displayingWeather (data) { 
-    cityNameEle.innerHTML = `${data.name}, ${data.sys.country}`;
-    cityTempEle.innerHTML = `${Math.round(data.main.temp)}<span class="dot">&#8728;C</span>`;
-    cityWeatherEle.innerHTML = `${data.weather[0].main}`;
-    humidity.innerHTML = `${data.main.humidity} %`;
-    windSpeed.innerHTML = `${Math.round((data.wind.speed * 3600) / 1000)} Km/h`;
-    weatherImage.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`);
-}
-async function gettingCurrentWeather(cityName) {
-    const response = await fetch(`${baseUrl}weather?q=${cityName}&appid=20bce2cb30ef0934691dcac34c7f8f0c&units=metric`);
-    if (response.status !== 200) {
-        cityInputField.value = "";
-    } else {
-        const data = await response.json();
-        displayingWeather(data);
-    }
-}
-cities.forEach(city => {
-    let cityName = city.innerHTML;
-    city.addEventListener("click", () => {
-        gettingCurrentWeather(cityName)
-    })
 })
 
-if (cityInputField.value === "") {
-    gettingCurrentWeather(cities[0].innerHTML)
+function restartGame () {
+    currSign = "";
+    zeroArr = [];
+    crossArr = []
+    numOfSlots = 9;
+    boxes.forEach(box => {
+        box.innerHTML = "";
+        box.setAttribute("isClicked", "false");
+    });
+    restartBtn.parentElement.style.display = "none";
+    showSignSeclecingSec();
 }
-searchBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    let cityName = cityInputField.value;
-    cityName = cityName.replaceAll(" ", '')
-    validatingCityName(cityName);
-})
+function checkWinner (arr) {
+    const methods = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];            
+
+    for (let method of methods) {
+        if (method.every(slot => arr.includes(slot))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function displayingSigns (e, num) {
+    numOfSlots--;
+    if (currSign === "cross") {
+        e.target.innerHTML = `<div class="cross"></div>`;
+        crossArr.push(num);
+        currSign = "zero";
+        if (crossArr.length > 2 && checkWinner(crossArr)) {; 
+            crossScore.textContent = `${parseInt(crossScore.textContent) + 1}`;
+        }
+    } else if (currSign === "zero") {
+        e.target.innerHTML = `<div class="circle"></div>`;
+        zeroArr.push(num);
+        currSign = "cross";
+        if (zeroArr.length > 2 && checkWinner(zeroArr)) {
+            zeroScore.textContent = `${parseInt(zeroScore.textContent) + 1}`;
+        }
+    }
+    if (checkWinner(crossArr))  {
+        restartBtn.parentElement.style.display = "block";
+        winMessage.innerHTML = `X Wins`;
+    } else if (checkWinner(zeroArr)) {
+        restartBtn.parentElement.style.display = "block";
+        winMessage.innerHTML = `O Wins`;
+    } else if (numOfSlots === 0) {
+        restartBtn.parentElement.style.display = "block";
+        winMessage.innerHTML = `Draw`;
+    } 
+}
+restartBtn.addEventListener("click", restartGame);
+
+boxesContainer.addEventListener("click", (e) => {
+    if (currSign) {
+        let isClicked = e.target.getAttribute("isClicked");
+        if (isClicked === "false") {
+            const boxNum = parseInt(e.target.id.replace("box", ""));
+            displayingSigns(e, boxNum);
+            e.target.setAttribute("isClicked", "true");
+        } 
+    }
+});
