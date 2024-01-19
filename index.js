cfa96d7a6b8ce170da94ed161020ea8de118b0c3
Abbas 01 <abbashere786@gmail.com>
2024-01-19 16:45:27 +0500

    Getting the reference of object constructor function using constructor property

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 function UsersDetails (name, age, password) {
     this.name =  name;
     this.age = age;
-    this.password = password
+    this.password = password;
+    this.greetings = function() {
+        return `Hello, My name is ${name}`;
+    }
 }
 const userOne = new UsersDetails("Abbas", 16, "Hello_World!");
 const userTwo = new UsersDetails("Asad", 16, "Hello_Universe!");
 
-console.log(userOne.name);
-console.log(userTwo.name);
+console.log(userOne.constructor);
+console.log(userOne.greetings());
+console.log(userTwo.greetings());
