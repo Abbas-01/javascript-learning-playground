6919aca614b15cb6daadc4286057e8742d6c8e04
Abbas 01 <abbashere786@gmail.com>
2024-01-22 20:55:06 +0500

    Using static method

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 class Users {
-    constructor(userName, email, age) {
+    constructor(userName, email, password, age) {
         this.userName = userName;
         this.email = email;
+        this.password = this.password;
         this.age = age;
     }
     intro () {
         return `Hello! my name is ${this.userName}`
     }
+    valThis () {
+        return this
+    }
+    static userPassword() {
+        return `${this.password}`;
+    }
 }
 
-const userOne = new Users("Abbas", "abbashere786@gmail.com", 16);
-console.log(userOne.userName)
-console.log(userOne.intro())
-console.log(userOne.email)
+const userOne = new Users("Abbas", "abbashere786@gmail.com", "atsa1122", 16);
+console.log(userOne.userName);
+console.log(userOne.intro());
+console.log(userOne.email);
+console.log(userOne.valThis());
+console.log(userOne.userPassword())
